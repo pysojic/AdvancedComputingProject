a3d3bbce6a4c14a6d0bc62b962a1597b858d75e9
pysojic <pysojic@uchicago.edu>
2025-04-26 17:13:32 -0500

    market snapshot

--- Part3/include/market_snapshot.h
+++ Part3/include/market_snapshot.h
@@ -18,13 +18,13 @@ public:
     { 
         if (m_Bids.contains(price))
             m_Bids[price]->m_Quantity += qty;
-        m_Bids.try_emplace(price, std::make_unique<PriceLevel>(price, qty)); 
+        m_Bids.try_emplace(price, std::make_unique<PriceLevel>(price, qty));
     }
     void update_ask(double price, int qty) 
     { 
         if (m_Asks.contains(price))
             m_Asks[price]->m_Quantity += qty;
-        m_Asks.try_emplace(price, std::make_unique<PriceLevel>(price, qty)); 
+        m_Asks.try_emplace(price, std::make_unique<PriceLevel>(price, qty));
     }
     const PriceLevel& get_best_ask() const { return *m_Asks.begin()->second; }
     const PriceLevel& get_best_bid() const { return *m_Bids.begin()->second; }
