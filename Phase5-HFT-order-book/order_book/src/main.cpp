8b606c444e27a10ef2337b0def7e91d27caf1e4f
pysojic <pysojic@uchicago.edu>
2025-05-11 23:44:12 -0500

    push

--- Phase5-HFT-order-book/order_book/src/main.cpp
+++ Phase5-HFT-order-book/order_book/src/main.cpp
@@ -3,23 +3,6 @@
 #include "../include/benchmark.hpp"
 #include "../include/unit_tests.hpp"
 
-#include <random>
-
-template <typename T>
-void stressTest(T& book, int numOrders) {
-    std::mt19937 rng(std::random_device{}());
-    std::uniform_real_distribution<double> priceDist(50.0, 100.0);
-    std::uniform_int_distribution<int> quantityDist(1, 500);
-
-    for (int i = 0; i < numOrders; ++i) {
-        std::string id = "ORD" + std::to_string(i);
-        double price = priceDist(rng);
-        int quantity = quantityDist(rng);
-
-        book.addOrder(id, price, quantity, true);
-    }
-}
-
 int main()
 {   
     // Run unit tests
