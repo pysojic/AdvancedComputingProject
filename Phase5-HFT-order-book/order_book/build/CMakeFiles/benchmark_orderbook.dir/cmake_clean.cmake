46df33bc871a007ea0c15137c02ccc5e6b23bb4b
pysojic <pysojic@uchicago.edu>
2025-05-11 23:31:49 -0500

    push

---
+++ Phase5-HFT-order-book/order_book/build/CMakeFiles/benchmark_orderbook.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/benchmark_orderbook.dir/src/main.cpp.o"
+  "CMakeFiles/benchmark_orderbook.dir/src/main.cpp.o.d"
+  "benchmark_orderbook"
+  "benchmark_orderbook.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/benchmark_orderbook.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Phase5-HFT-order-book/order_book/src/main.cpp
+++ Phase5-HFT-order-book/order_book/src/main.cpp
@@ -3,6 +3,23 @@
 #include "../include/benchmark.hpp"
 #include "../include/unit_tests.hpp"
 
+#include <random>
+
+template <typename T>
+void stressTest(T& book, int numOrders) {
+    std::mt19937 rng(std::random_device{}());
+    std::uniform_real_distribution<double> priceDist(50.0, 100.0);
+    std::uniform_int_distribution<int> quantityDist(1, 500);
+
+    for (int i = 0; i < numOrders; ++i) {
+        std::string id = "ORD" + std::to_string(i);
+        double price = priceDist(rng);
+        int quantity = quantityDist(rng);
+
+        book.addOrder(id, price, quantity, true);
+    }
+}
+
 int main()
 {   
     // Run unit tests
@@ -12,8 +29,16 @@ int main()
     OrderBookTests::runTests<OptimizedOrderBook>();
     std::cout << "All tests passed!" << std::endl;
 
+    stressTest(optimized_ob, 100);
+
     // Run benchmark
     std::cout << "Running benchmark..." << std::endl;
+
+    benchmark(1000);
+    benchmark(5000);
+    benchmark(10000);
+    benchmark(50000);
     benchmark(100000);
+
     return 0;
 }
