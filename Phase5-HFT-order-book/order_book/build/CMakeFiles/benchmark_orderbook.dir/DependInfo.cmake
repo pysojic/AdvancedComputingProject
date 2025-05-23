1417cbb40b17fd9872c18361c4e91a87ba64cf21
sxcben <benjamingerino@gmail.com>
2025-05-22 23:06:39 -0500

    added benjamin cmake

---
+++ Phase5-HFT-order-book/order_book/build/CMakeFiles/benchmark_orderbook.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/cygdrive/c/Users/benja/OneDrive/Bureau/UchicagoMSFM/advanced_computing/AdvancedComputingProject/Phase5-HFT-order-book/order_book/src/main.cpp" "CMakeFiles/benchmark_orderbook.dir/src/main.cpp.o" "gcc" "CMakeFiles/benchmark_orderbook.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Phase5-HFT-order-book/order_book/build/CMakeFiles/benchmark_orderbook.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/benchmark_orderbook.dir/src/main.cpp.o"
+  "CMakeFiles/benchmark_orderbook.dir/src/main.cpp.o.d"
+  "benchmark_orderbook.exe"
+  "benchmark_orderbook.pdb"
+  "libbenchmark_orderbook.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/benchmark_orderbook.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
